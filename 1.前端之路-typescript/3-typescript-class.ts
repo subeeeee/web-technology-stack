c87c287e458a247fcafd145a41642cc9eb41044e
wangzhen <wangzhen@situdata.com>
2020-07-21 08:58:20 +0800

    快速排序逻辑

--- 1.前端之路-typescript/3-typescript-class.ts
+++ 1.前端之路-typescript/3-typescript-class.ts
@@ -1,7 +1,9 @@
 /*
 * ts中实现函数继承， 类 及原理
 * */
-
+/**
+ * 类的声明
+ * */
 class Persons {
   name: string;
   age: number | undefined;
@@ -16,7 +18,53 @@ class Persons {
     return this.age
   }
 }
-const wz = new Persons("王震",24)
-console.log(wz.getAge())
-console.log(wz.getName())
+const wz1 = new Persons("王震",24)
+// console.log(wz1.getAge())                          // 24
+// console.log(wz1.getName())                         // 王震
 
+/**
+ * 类的继承
+ * */
+class Men extends Persons{
+  sex: string;
+  constructor(name: string, age: number, sex: string) {
+    super(name,age);
+    this.sex = sex;
+  }
+  getSex(): string {
+    return  this.sex
+  }
+}
+const wz2 = new Men('王震', 24, '男')
+
+
+// console.log(wz2.getAge())                              // 24
+// console.log(wz2.getName())                             // 王震
+// console.log(wz2.getSex())                              // 男
+
+
+/**
+ * typeScript 中公共，私有与受保护的修饰符
+ *
+ *
+ * @public       属性不定义类型 默认是public
+ *
+ * @private     当成员被标记成 private 时，它就不能在声明它的类的外部访问
+ * */
+
+class Animal1 {
+  private type: string
+  private name: string
+  private age: number
+  protected con: string
+  readonly  d: string
+
+  constructor(type: string, name: string, age: number) {
+    this.type = type
+    this.name = name
+    this.age = age
+  }
+  getName(): string {
+    return  this.name
+  }
+}
